e24691b87913db10895ff8f3c0b902a67fbb6289
adopWesley <wesley@adop.cc>
2022-04-06 23:27:48 +0900

    비접속중인 유저 제거

--- src/main/java/com/wesley/websocket/Controller/MessageController.java
+++ src/main/java/com/wesley/websocket/Controller/MessageController.java
@@ -7,13 +7,10 @@
  */
 package com.wesley.websocket.Controller;
 
-import com.wesley.websocket.VO.User;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.persistence.Basic;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -22,8 +19,6 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * create on 2021/04/13.
@@ -42,6 +37,10 @@ import java.util.concurrent.CopyOnWriteArraySet;
 public class MessageController extends Socket {
     private static final List<Session> session = new ArrayList<Session>();
 
+    public MessageController() {
+//        this.isSessionClosed();
+    }
+
     @GetMapping("/")
     public String index() {
         return "index.html";
@@ -51,25 +50,40 @@ public class MessageController extends Socket {
     public void open(Session newUser) {
         System.out.println("connected");
         session.add(newUser);
-        System.out.println("현재 접속중인 유저 수 : "+ session.size());
+        System.out.println("현재 접속중인 유저 수 : " + session.size());
     }
 
     @OnMessage
     public void getMsg(Session recieveSession, String msg) {
-//            recieveSession.getBasicRemote().sendText(msg);
         for (int i = 0; i < session.size(); i++) {
-            if (!recieveSession.getId().equals(session.get(i).getId())) {
+            if (! recieveSession.getId().equals(session.get(i).getId())) {
                 try {
-                    session.get(i).getBasicRemote().sendText("상대 : "+msg);
+                    session.get(i).getBasicRemote().sendText("유저" + (Integer.parseInt(session.get(i).getId()) + 1) + " : " + msg);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }else{
+            } else {
                 try {
-                    session.get(i).getBasicRemote().sendText("나 : "+msg);
+                    session.get(i).getBasicRemote().sendText("나 : " + msg);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    @Scheduled(cron = "* * * * * *")
+    private void isSessionClosed() {
+        if (session.size() != 0) {
+            try {
+                System.out.println("! = " + session.size());
+                for (int i = 0; i < session.size(); i++) {
+                    if (! session.get(i).isOpen()) {
+                        session.remove(i);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
--- src/main/java/com/wesley/websocket/WebSocketApplication.java
+++ src/main/java/com/wesley/websocket/WebSocketApplication.java
@@ -3,9 +3,11 @@ package com.wesley.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
+@EnableScheduling
 public class WebSocketApplication {
 
     public static void main(String[] args) {
